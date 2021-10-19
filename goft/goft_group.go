bdfc46f811189480ff5121282dc349de52af9ec3
tangx <uyinn@live.com>
2021-10-19 14:39:59 +0800

    docs: 增加 comment

--- goft/goft_group.go
+++ goft/goft_group.go
@@ -6,18 +6,21 @@ type GoftGroup struct {
 	*gin.RouterGroup
 }
 
+// baseGoftGroup 通过 Goft 返回一个根 GoftGroup
 func baseGoftGroup(r *Goft, group string) *GoftGroup {
 	return &GoftGroup{
 		RouterGroup: r.Group(group),
 	}
 }
 
+// newGoftGroup 通过 GoftGroup 扩展新的 GoftGroup
 func newGoftGroup(base *GoftGroup, group string) *GoftGroup {
 	return &GoftGroup{
 		RouterGroup: base.Group(group),
 	}
 }
 
+// Mount 在 GoftGroup 上绑定/注册 控制器
 func (gg *GoftGroup) Mount(group string, claess ...ClassController) *GoftGroup {
 	grp := newGoftGroup(gg, group)
 
